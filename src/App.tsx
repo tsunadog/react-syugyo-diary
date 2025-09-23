2d9f0e338a9d32bf0dc5142ad10ceeecde79e5e6
tsunadog <tsunadog27@gmail.com>
2025-09-23 14:59:31 +0900

    Day2:useEffect と依存配列（APIからデータ取得）

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import One from "./pages/01";
 import Index from "./Index";
+import Two from "./pages/02";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="01" element={<One />} />
+        <Route path="02" element={<Two />} />
       </Routes>
     </BrowserRouter>
   );
--- src/data/page.tsx
+++ src/data/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import One from "../pages/01.tsx";
+import Two from "../pages/02.tsx";
 
 type pageType = {
   path: string;
@@ -12,5 +13,10 @@ export const page: pageType[] = [
     path: "01",
     page: <One />,
     title: "Day1：useState の型付けと初期値（カウンターアプリ）",
+  },
+  {
+    path: "02",
+    page: <Two />,
+    title: "Day2:useEffect と依存配列（APIからデータ取得）",
   },
 ];
